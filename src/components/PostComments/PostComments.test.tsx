30bde06b04db9c758678a15a34641a1cab6a929d
albert <albertsoares97@gmail.com>
2023-07-26 18:58:01 -0300

    implementa teste insere dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,37 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
+// import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
+// describe('Teste para o componente PostComment', () => {
+//     it('Deve renderizar o componente corretamente', () => {
+//         render(<PostComment/>);
+//         expect(screen.getByText('Comentar')).toBeInTheDocument();
+//     });
+// });
+
+describe('Teste para o componente Post', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<Post />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<Post />);
+
+        // Encontra o elemento do textarea e o botão de envio
+        const textareaElement = screen.getByRole('textbox');
+        const submitButton = screen.getByText('Comentar');
+
+        // Digita e envia o primeiro comentário
+        fireEvent.change(textareaElement, { target: { value: 'Comentário 1' } });
+        fireEvent.click(submitButton);
+
+        // Digita e envia o segundo comentário
+        fireEvent.change(textareaElement, { target: { value: 'Comentário 2' } });
+        fireEvent.click(submitButton);
+
+        // Verifica se os comentários foram renderizados corretamente usando o atributo key (id)
+        expect(screen.getByText('Comentário 1')).toBeInTheDocument();
+        expect(screen.getByText('Comentário 2')).toBeInTheDocument();
     });
 });
